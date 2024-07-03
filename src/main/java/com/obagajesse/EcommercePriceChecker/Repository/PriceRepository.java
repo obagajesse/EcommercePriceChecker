f1c2b3831f0308b8c6f68a6f10e11ce5017a8124
obagajesse <obagajesse@gmail.com>
2024-07-03 17:58:18 +0300

    EcommercePriceChecker

---
+++ src/main/java/com/obagajesse/EcommercePriceChecker/Repository/PriceRepository.java
@@ -0,0 +1,9 @@
+package com.obagajesse.EcommercePriceChecker.Repository;
+
+import com.obagajesse.EcommercePriceChecker.Entities.Price;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PriceRepository extends JpaRepository<Price,Long> {
+}
