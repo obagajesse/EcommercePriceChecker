fb10ab0e238b799125ba4aa51cb325774fda33b8
obagajesse <obagajesse@gmail.com>
2024-07-03 18:18:28 +0300

    EcommercePriceChecker

---
+++ src/main/java/com/obagajesse/EcommercePriceChecker/Service/PriceService.java
@@ -0,0 +1,20 @@
+package com.obagajesse.EcommercePriceChecker.Service;
+
+import com.obagajesse.EcommercePriceChecker.DTO.PriceDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface PriceService {
+
+    PriceDTO createPrice(PriceDTO priceDTO);
+
+    PriceDTO updatePrice(PriceDTO priceDTO);
+
+    PriceDTO getPriceById(Long id);
+
+    List<PriceDTO> getAllPrices();
+
+    void deletePrice(Long id);
+}
