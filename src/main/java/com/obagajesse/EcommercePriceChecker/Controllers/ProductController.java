471e4f2b6b8345b2d3a9b643323bcc7c24ea5102
obagajesse <obagajesse@gmail.com>
2024-07-03 09:40:05 +0300

    EcommercePriceChecker

--- src/main/java/com/obagajesse/EcommercePriceChecker/Controllers/ProductController.java
+++ src/main/java/com/obagajesse/EcommercePriceChecker/Controllers/ProductController.java
@@ -1,8 +1,12 @@
 package com.obagajesse.EcommercePriceChecker.Controllers;
 
+import com.obagajesse.EcommercePriceChecker.DTO.ProductDTO;
 import com.obagajesse.EcommercePriceChecker.Service.ProductService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
@@ -14,5 +18,26 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<ProductDTO>> getAllProducts(){
+        List<ProductDTO> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getProductById(@PathVariable Long id){
+        ProductDTO productDTO = productService.getProductById(id);
+        return ResponseEntity.ok(productDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO productDTO){
+        return new ResponseEntity<>(productService.createProduct(productDTO), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ProductDTO> deleteProduct(@PathVariable Long id){
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
